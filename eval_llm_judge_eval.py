5121b39a5c505af042adc58eec157a024240e727
Nguyen Quang Dung <quangdungnguyen2004@gmail.com>
2026-04-28 19:19:55 +0700

    do llm judge, finish gemma

--- eval_llm_judge_eval.py
+++ eval_llm_judge_eval.py
@@ -297,9 +297,9 @@ def save_report(report: Dict[str, Any], output_path: str) -> None:
 
 if __name__ == "__main__":
     benchmark_path = "./eval/eval_dataset_full.json"
-    agent_output_path = "./eval/agent_output_oss20_4fewshot.json"
+    agent_output_path = "./eval/agent_output_gemma4_4fewshot.json"
     system_prompt_path = "./eval/llm_judge_sysprompt.txt"
-    output_path = "./eval/llm_judge_report_oss20_4fewshot_3.json"
+    output_path = "./eval/llm_judge_report_gemma4_4fewshot_3.json"
     tool_list = "./tool_list.json"
 
     report = run_llm_judge_eval(
